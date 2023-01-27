6789d6ddc67ebbb462d1948ecba978a1b9b8efc6
DanOlinski <daniel.guterres@hotmail.com>
2023-01-26 17:09:40 -0700

    asserEqual function

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+let assertEqual = function(actual, expected) {
+  let final;
+ 
+  if (actual === expected) {
+    final = `😎😎😎Assertion Passed: ${actual} === ${expected}`;
+  } else {
+    final = `😣😣😣Assertion Failed: ${actual} !== ${expected}`;
+  }
+ 
+  console.log(final);
+};
+
+assertEqual('Lighhouse Labs', 'Bootcamp');
+assertEqual(1, 1);
